e0a4828b3d89d18f7253f58ac25001b43d26dd89
Matthew Aguirre <matt.aguirre@gmail.com>
2015-05-21 16:26:21 -0400

    Updated to be able to upload to maven. Added updated by me to comments.

--- src/org/graphstream/stream/gephi/JSONReceiver.java
+++ src/org/graphstream/stream/gephi/JSONReceiver.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012 wumalbert
+ * Updated by Matt Aguirre <matta@artistech.com>
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
  * associated documentation files (the "Software"), to deal in the Software without restriction, 
